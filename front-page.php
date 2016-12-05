085c1c6dbf68a246648d184948dcb83b9eb3aba5
Elaine Broussard <elaine@elainebroussard.com>
2016-12-04 20:04:14 -0600

    add content and styles to center of front page

--- front-page.php
+++ front-page.php
@@ -22,6 +22,28 @@
 			</ul>
 		</div>
 	</section>
+	<section id="frontpage-middle" class="row">
+		<div class="four columns">
+			<img src="wp-content/uploads/2016/12/image-1.jpg" alt="qualified technicians" title="qualified technicians" width="295">
+			<h1>Industry Certified Techs</h1>
+			<p>Our team is made up of the most knowledgeably trained crew in the area; a crew that has been trained by world recognizable institutions in all aspects of automotive collision and mechanical repair. As for training, it never ends for our crew. Our team maintains a continuous training program to stay ahead of new innovations in the automotive repair industry.</p>
+			<p class="calltoaction">Learn How We Got Started</p>
+		</div>
+		<div class="four columns">
+			<img src="wp-content/uploads/2016/12/image-2.jpg" alt="latest technology" title="latest technology" width="295">
+			<h1>State of the Art Technology</h1>
+			<p>We employ today’s latest automotive technology and are equipped to handle all major and minor repairs on foreign and domestic vehicles. No repair is too complicated for us. We constantly keep our tools ane equipment updated, so our customers can always enjoy cutting edge service. Because of this, we are able to tackle difficult and complicated jobs that many of our competitors cannot.
+			<p class="calltoaction">View Our Price Sheet</p>
+		</div>
+		<div class="four columns">
+			<img src="wp-content/uploads/2016/12/image-3.jpg" alt="great customer service" title="great customer service" width="295">
+			<h1>Superb Customer Service</h1>
+			<p>When you have to be inconvenienced with automobile repairs, bring your vehicle in to a place that makes you feel welcomed and comfortable; a place which keeps you informed on your vehicle’s progress; a place where people are willing to advise you with knowledgeable details; a place where you know they will provide high quality work and guarantee what they deliver.</p>
+			<p class="calltoaction">Contact Us Today</p>
+		</div>
+	</section>
+	<section class="row">
+	</section>
 <!-- End Section Container-->
 
 <?php get_footer(); ?>
