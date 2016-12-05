48850e3a2afe13ca6d819d9efab7f476682672fa
Elaine Broussard <elaine@elainebroussard.com>
2016-12-05 14:03:14 -0600

    establish default page styles

--- page.php
+++ page.php
@@ -9,7 +9,7 @@
 				if ( have_posts() ) {
 					while ( have_posts() ) {
 						the_post(); ?>
-						<h2><?php the_title(); ?></h2>
+						<h2 class="pagetitle"><?php the_title(); ?></h2>
 						<?php the_content(); ?>				
 					<?php	
 					} //end while
